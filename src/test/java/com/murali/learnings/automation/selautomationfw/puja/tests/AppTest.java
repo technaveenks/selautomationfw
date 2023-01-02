47fc6ea1fe96b178de5b73a7f83a1cee44750097
Deepthi <tech.naveenks@gmail.com>
2023-01-02 20:49:53 +0530

    createdpackage -puja

---
+++ src/test/java/com/murali/learnings/automation/selautomationfw/puja/tests/AppTest.java
@@ -0,0 +1,18 @@
+package com.murali.learnings.automation.selautomationfw.puja.tests;
+
+
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+   // @Test
+    public void shouldAnswerWithTrue()
+    {
+        //assertTrue( true );
+    }
+}
---
+++ src/test/java/com/murali/learnings/automation/selautomationfw/puja/tests/SmokeTest.java
@@ -0,0 +1,23 @@
+package com.murali.learnings.automation.selautomationfw.puja.tests;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.testng.annotations.Test;
+
+import com.murali.learnings.automation.selautomationfw.utils.BaseTest;
+
+public class SmokeTest extends BaseTest {
+	//Page Object
+	@FindBy(name = "search")
+	private WebElement _txtSearch;
+
+	@Test(groups = { "smoke", "sanity" })
+	public void verifySearchOfItems() {
+		getDriver().findElement(By.name("search")).clear();
+		getDriver().findElement(By.name("search")).sendKeys("iPhone");
+		getDriver().findElement(By.xpath("//*[@id=\"search\"]/span/button")).click();
+
+	}
+
+}
